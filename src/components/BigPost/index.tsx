52ca6fa18d0aa0e3957461675a9b882bffb37d31
caioaugustoo <caioamfr@gmail.com>
2021-03-16 15:49:45 -0300

    Criado componente de post do maior tamanho

---
+++ src/components/BigPost/index.tsx
@@ -0,0 +1,37 @@
+import * as S from './styles'
+import Link from 'next/link'
+
+export type BigPostProps = {
+  title: string
+  date: string
+  excerpt: string
+  slug: string
+  imageUrl: string
+}
+
+const BigPost = ({ title, date, excerpt, imageUrl, slug }: BigPostProps) => (
+  <S.Wrapper>
+    <Link href={slug}>
+      <a aria-label={title}>
+        <S.CoverImage src={imageUrl} alt={title} />
+      </a>
+    </Link>
+
+    <S.PostContent>
+      <div>
+        <S.PostTitle>
+          <Link href={slug}>
+            <a aria-label={title} title={title}>
+              {title}
+            </a>
+          </Link>
+        </S.PostTitle>
+
+        <S.Date>{date}</S.Date>
+      </div>
+      <S.PostExcerpt>{excerpt}</S.PostExcerpt>
+    </S.PostContent>
+  </S.Wrapper>
+)
+
+export default BigPost
