0ebf078d2f32f40523a16b2f1bb7ad3d5f44c0c4
caioaugustoo <caioamfr@gmail.com>
2021-03-18 01:20:40 -0300

    Criada estrutura do Post e Home

--- src/Templates/Home/styles.tsx
+++ src/Templates/Home/styles.tsx
@@ -6,9 +6,57 @@ export const HeroSection = styled.section`
   `}
 `
 
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin: 0 10px;
+    }
+  }
+`
+
 export const MorePostSection = styled.section`
   ${({ theme }) => css`
     margin: ${theme.spacings.xxlarge} auto ${theme.spacings.medium} auto;
+
+    > div > div:first-child {
+      max-width: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+
+      > .content {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        margin: ${theme.spacings.large} 0;
+        grid-gap: 30px;
+
+        @media (max-width: 765px) {
+          grid-template-columns: 1fr;
+          grid-gap: 20px;
+        }
+
+        .infos h3 {
+          font-size: ${theme.font.sizes.xxlarge};
+          line-height: 1.2;
+          margin-bottom: ${theme.spacings.xsmall};
+          font-weight: ${theme.font.normal};
+
+          @media (max-width: 765px) {
+            font-size: ${theme.font.sizes.xlarge};
+          }
+
+          &:hover a {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
 
     > div {
       display: flex;
--- src/Templates/Post/index.tsx
+++ src/Templates/Post/index.tsx
@@ -5,30 +5,38 @@ import { PostsProps } from 'pages/post/[slug]'
 
 import * as S from './styles'
 import Link from 'next/link'
+import DarkModeButton from 'components/DarkmodeButton'
+import Head from 'components/Head'
 
 const PostTemplate = ({ posts }: PostsProps) => {
   return (
-    <S.Wrapper>
-      <Container>
-        <S.BackToHome>
-          <Link href="/">
-            <a
-              title="Voltar para página inicial"
-              aria-label="Voltar para página inicial"
-            >
-              <Heading>Início</Heading>
-            </a>
-          </Link>
-        </S.BackToHome>
-
-        <Heading size="huge">{posts.title}</Heading>
-
-        <S.PostContentWrapper>
-          <S.CoverImage src={posts.coverImage.url} alt={posts.title} />
-          <S.Content dangerouslySetInnerHTML={{ __html: posts.content.html }} />
-        </S.PostContentWrapper>
-      </Container>
-    </S.Wrapper>
+    <>
+      <Head title={posts.seo.title} description={posts.seo.description} />
+      <S.Wrapper>
+        <Container>
+          <S.Header>
+            <nav>
+              <Link href="/">
+                <a
+                  title="Voltar para página inicial"
+                  aria-label="Voltar para página inicial"
+                >
+                  <Heading>Início</Heading>
+                </a>
+              </Link>
+            </nav>
+            <DarkModeButton />
+          </S.Header>
+          <Heading size="huge">{posts.title}</Heading>
+          <S.PostContentWrapper>
+            <S.CoverImage src={posts.coverImage.url} alt={posts.title} />
+            <S.Content
+              dangerouslySetInnerHTML={{ __html: posts.content.html }}
+            />
+          </S.PostContentWrapper>
+        </Container>
+      </S.Wrapper>
+    </>
   )
 }
 
